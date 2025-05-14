3e4309d383efa4aa8c99b72c96ee519064b4060e
Alex Mihov <alexmihov.as@gmail.com>
2025-05-14 12:05:03 +0200

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,54 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([])
 
+  function addToCart(product) {
+
+    const trovato = addedProducts.find(p => p.name === product.name)
+
+    if (!trovato) {
+      const newProduct = { ...product, quantity: 1 }
+      setAddedProducts([...addedProducts, newProduct])
+    }
+
+    console.log(addedProducts);
+
+
+  }
 
   return (
     <>
-      <ul>
-        {products.map((p, index) => (
-          <li key={index}>
-            <span>{p.name}</span> - <span>{p.price}€</span>
+      <div>
 
-          </li>
-        ))}
-      </ul>
+        <h3>Lista prodotti</h3>
+
+        <ul>
+          {products.map((product, index) => (
+            <li key={index}>
+              <span>{product.name}</span> - <span>€{product.price}</span>
+              <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
+            </li>
+          ))}
+        </ul>
+      </div>
 
 
+
+      {addedProducts.length > 0 && (
+        <div>
+
+          <h3>Carrello</h3>
+
+          <ul>
+            {addedProducts.map((item, index) => (
+              <li key={index}>
+                {item.name} - €{item.price} - Quantità: {item.quantity}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   )
 }
